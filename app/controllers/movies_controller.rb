edc6f47bf21029c7c82e82fd0a56f53d180b6691
Jendrik Mallah <jm.orders+lewagon@outlook.com>
2024-02-09 17:19:03 +0100

    deleted some raises and made comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,13 @@ class MoviesController < ApplicationController
   before_action :set_questionnaire, only: [:create]
 
   def create
+    # questionnaires results is an array of strings
+    # eval will create a hash from the string elements that were saved in the results
     @movie = eval(@questionnaire.results.first)
     @movie_db = Movie.new
 
+    # since movie has a lot more keys from the api, we need to check if
+    # the key is also available in our movie model, if so: write the attribute
     @movie.each do |key, value|
       if Movie.column_names.include?(key) && key != "id"
         @movie_db[key] = value
@@ -13,12 +17,10 @@ class MoviesController < ApplicationController
     # raise
     if @movie_db.save
       @watched_movie = WatchedMovie.new(user_id: current_user.id, movie: @movie_db)
-      raise
       @watched_movie.save
     else
-      render :new
+      # render :new
     end
-    raise
   end
 
   private
