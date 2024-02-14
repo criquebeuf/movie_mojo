16240b68f72074aaeea865c970042d844866d0fe
Jendrik Mallah <jm.orders+lewagon@outlook.com>
2024-02-14 13:09:33 +0100

    updated favicon, bug in dropdown, updated meta tags and custom error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
     stored_location_for(resource_or_scope) || super
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def storable_location?
