84f04b245468b72c3d2f75198861c3df51f7fea6
Jendrik Mallah <jm.orders+lewagon@outlook.com>
2024-02-14 17:09:19 +0100

    we have filled dots

--- app/javascript/controllers/question_controller.js
+++ app/javascript/controllers/question_controller.js
@@ -2,32 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="question"
 export default class extends Controller {
-  static targets = ["answer"]
+  static targets = ["answer", "fill"]
+  static values = {
+    qindex: Number
+  }
+
+  connect() {
+    console.log(this.qindexValue)
+
+    const dots = this.fillTarget.getElementsByClassName("dot");
+    var question_index = this.qindexValue + 1
+    // console.log(dots)
+    // console.log(event.target.dataset.qindex)
+
+    // getElementsByClassName provides HTML collection instead of array
+    // so we need to convert it to an array with Array.from()
+    Array.from(dots).forEach((dot) => {
+      if (question_index > 0) {
+        dot.classList.add("completed");
+        question_index--
+      }
+    });
+  }
 
   next() {
     // for animation
     this.element.classList.remove("previous");
-
-    // console.dir(this.answerTarget)
-
-    // this.answerTarget = localStorage.getItem("answer");
-
-    // if (localStorage.getItem("baseURI") === this.element.baseURI) {
-    //   console.log("helloooo, I am the same")
-    // }
   }
 
   previous() {
     // for animation
     this.element.classList.add("previous");
-    console.log("previous")
-
-    localStorage.setItem("answer", this.answerTarget.value);
   }
 
   storeData() {
-  //   console.dir(this.answerTarget);
+    // was just for testing to store data for unfinished questions
+    // console.dir(this.answerTarget);
 
-  //   console.log(localStorage.getItem("baseURI"));
+    // console.log(localStorage.getItem("baseURI"));
   }
 }
