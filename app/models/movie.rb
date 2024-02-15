3b7753cacd750fd1429d045e61d6ec930fd8e113
Pelayo de las Heras <pelayodlh@gmail.com>
2024-02-15 11:58:13 +0100

    Search bar implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ApplicationRecord
   has_many :watched_movies
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_overview,
+  against: [ :title, :overview ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
